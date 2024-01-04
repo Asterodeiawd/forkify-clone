f0e693627e92d088ca98fefd8b4c06631a0fbfe6
asterodeia <d_wang_890227@outlook.com>
2024-01-04 14:54:30 +0800

    finish section serving

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,12 @@
+"use strict";
+
 const API_END_POINT = "https://forkify-api.herokuapp.com/api/v2/recipes";
-console.log("es module loaded");
+const API_KEY = "73fb1d7f-54ab-4ab9-87cb-790d349942a5";
 
 const getAllRecipes = async keyWord => {
   try {
     const resp = await fetch(
-      `${API_END_POINT}?search=${keyWord}&key=73fb1d7f-54ab-4ab9-87cb-790d349942a5`
+      `${API_END_POINT}?search=${keyWord}&key=${API_KEY}`
     );
 
     const results = await resp.json();
@@ -17,7 +19,6 @@ const getAllRecipes = async keyWord => {
 
 const addRecipeBrief = ({ id, title, publisher, image_url }) => {
   const pos = document.querySelector(".result-list");
-  console.log("pos", pos);
 
   pos.insertAdjacentHTML(
     "beforeend",
@@ -49,4 +50,55 @@ const getRecipeById = async id => {
   }
 };
 
-export { getRecipeById };
+// console.log(recipe);
+
+console.log(getRecipeById("5ed6604591c37cdc054bcb8a"));
+const recipeDetail = document.querySelector(".recipe");
+const recipe = {
+  publisher: "Real Simple",
+  ingredients: [
+    {
+      quantity: 0.75,
+      unit: "cup",
+      description: "chopped fresh pineapple",
+    },
+    {
+      quantity: 0.5,
+      unit: "cup",
+      description: "ice",
+    },
+  ],
+  source_url:
+    "http://www.realsimple.com/food-recipes/browse-all-recipes/carrot-pineapple-smoothie-00000000050727/index.html",
+  image_url:
+    "http://forkify-api.herokuapp.com/images/carrotsmoothie_30014096fc8.jpg",
+  title: "Carrot-Pineapple Smoothie",
+  servings: 4,
+  cooking_time: 45,
+  id: "5ed6604591c37cdc054bcb8a",
+};
+
+const showRecipeDetail = ({
+  id,
+  publisher,
+  ingredients,
+  source_url,
+  image_url,
+  title,
+  servings,
+  cooking_time,
+}) => {
+  const html = `
+         <figure class="recipe-detail-header">
+          <img
+            src=${image_url}
+            alt="recipe image"
+            class="recipe-detail-image"
+          />
+          <figcaption class="recipe-detail-title"><span>${title}</span></figcaption>
+        </figure>
+  `;
+  recipeDetail.insertAdjacentHTML("afterbegin", html);
+};
+
+showRecipeDetail(recipe);
